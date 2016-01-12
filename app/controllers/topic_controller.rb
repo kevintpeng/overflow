3d64ff544adff8e2c2a4c64a28da075ec338a995
Kevin Peng <kpeng97@gmail.com>
2016-01-12 14:01:24 -0500

    added topic controller

---
+++ app/controllers/topic_controller.rb
@@ -0,0 +1,9 @@
+class TopicController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+  
+  def show
+    @topic = Topic.find(params[:id])
+  end
+end
---
+++ test/controllers/topic_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TopicControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
