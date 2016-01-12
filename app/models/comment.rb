a3463dd0653d5f243cfbd7cfa076139cc4285750
Kevin Peng <kpeng97@gmail.com>
2016-01-12 13:06:21 -0500

    validation on comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   # polymorphic association allows comments to belong to more than one model
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  validates :reply, :presence => true
 end
