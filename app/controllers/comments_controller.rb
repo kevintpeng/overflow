bf3e472fd6d387ec2aba6f2bcf88a16126fe476f
Kevin Peng <kpeng97@gmail.com>
2016-02-03 00:49:37 -0500

    added polymorphic model creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,33 @@
 class CommentsController < ApplicationController
+  def create
+    @commentable = find_commentable
+    @comment = @commentable.comments.new
+    @comment.reply = params[:comment][:reply]
+    @comment.user_id = current_user.id
+    if @comment.save
+      flash[:success] = "Comment was added!"
+      redirect_to url_for @commentable
+    else
+      redirect_to :back
+    end
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:reply)
+  end
+
+  def find_commentable   # gets the type of comment to create
+    params.each do |name, value|
+      # matches the type of id using regex
+      if name =~ /(.+)_id$/
+        # returns the active record being referenced
+        # using constantize converts the string to any defined constant
+        return $1.classify.constantize.find(value)
+      end
+    end
+    nil
+  end
 end
