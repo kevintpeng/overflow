20ccdb9a687b1e8194aaa522f1bffabd8338d755
Kevin Peng <kpeng97@gmail.com>
2016-02-03 16:57:10 -0500

    views to add rep to answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -21,6 +21,7 @@ class AnswersController < ApplicationController
   def vote
     value = params[:type] == "up" ? 1 : -1
     @answer = Answer.find(params[:id])
-    @answer.add_evaluation :votes, value, current_user
+    @answer.add_or_update_evaluation :votes, value, current_user
     redirect_to :back, notice: "Thank you for voting"
+  end
 end
