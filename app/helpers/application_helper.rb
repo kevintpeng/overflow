8dcb858a3d06b0d0e36f955fa742adbb08ad6f6b
Kevin Peng <kpeng97@gmail.com>
2016-02-02 19:09:30 -0500

    devise changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # conversion of flash values to html class
+  def flash_class(level)
+    level = level.to_sym
+    case level
+    when :notice then "alert alert-info"
+    when :success then "alert alert-success"
+    when :error then "alert alert-danger"
+    when :alert then "alert alert-danger"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username, presence: :true
+  validates :username, presence: true, uniqueness: true
 
   has_many :questions
   has_many :answers
