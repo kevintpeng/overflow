13fcce941b4648d1708af9bfe401ebd55b2de6dc
Kevin Peng <kpeng97@gmail.com>
2016-02-03 19:11:18 -0500

    questions view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
+    # FIND A SORT BY REPUTATION
     @answers = @question.answers.find_with_reputation(:votes, :all, {:order => "votes DESC"})
     @answer = Answer.new
   end
