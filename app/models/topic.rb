383346df237a1c215da8aba71fb32bf1b7f977e1
Kevin Peng <kpeng97@gmail.com>
2016-02-05 10:48:55 -0500

    refactoring comment section

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,6 @@ class Topic < ActiveRecord::Base
   has_many :questions, dependent: :destroy
   has_many :tagging_relationships
   has_many :tags, :through => :tagging_relationships
+  has_many :comments, as: :commentable, dependent: :destroy
 
 end
