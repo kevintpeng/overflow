f6edd6eeff3b754d0e9c150a565879768ef9ee6d
Kevin Peng <kpeng97@gmail.com>
2016-02-06 14:54:22 -0500

    strong params dealt with

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,10 @@
 class AnswersController < ApplicationController
 
   def create
-    answer_params = params[:answer]
-    @question = Question.find answer_params[:question_id]
+    @question = Question.find params[:answer][:question_id]
 
     # TODO should refactor into standard strong params
-    @answer = @question.answers.new
-    @answer.response = answer_params[:response]
-    @answer.explanation = answer_params[:explanation]
+    @answer = @question.answers.new answer_params
     @answer.user = current_user
     if @answer.save
       flash[:success] = "Your answer was added."
@@ -18,10 +15,18 @@ class AnswersController < ApplicationController
     end
   end
 
+  # adjusts answer's score
   def vote
     value = params[:type] == "up" ? 1 : -1
     @answer = Answer.find(params[:id])
     @answer.add_or_update_evaluation :votes, value, current_user
     redirect_to :back, notice: "Thank you for voting"
   end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:response, :explanation)
+  end
+
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,4 +5,10 @@ class QuestionsController < ApplicationController
     @answers = @question.answers.find_with_reputation(:votes, :all, {:order => "votes DESC"})
     @answer = Answer.new
   end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:statement)
+  end
 end
