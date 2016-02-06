7669e4a6eb16f20c6c13369398452257cb7e0622
Kevin Peng <kpeng97@gmail.com>
2016-02-06 17:08:28 -0500

    rolledback one migration

--- db/migrate/20160206190733_create_tagging_relationships.rb
+++ db/migrate/20160206190733_create_tagging_relationships.rb
@@ -1,8 +1,6 @@
 class CreateTaggingRelationships < ActiveRecord::Migration
   def change
     create_table :tagging_relationships do |t|
-      # re-doing the tagging relationships table
-      drop_table :tagging_relationships
       t.references :tag, index: true, foreign_key: true
       t.references :topic, index: true, foreign_key: true
 
